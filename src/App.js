71b4dcc25deb2cc33ab7ac1506c2cda152b4e256
HarikowshikMolugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-04-30 16:56:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import  './components/about.css';
+import About from './components/about'; 
+
+
+function App() {
+  return (
+    <div className="App">
+        
+        <About></About>
+        
+      </div>
+    
+    
+  )
+
+}
+
+
+export default App;
+
